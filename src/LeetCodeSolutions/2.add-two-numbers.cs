576fb98c330c76d6808698bef0a43d409d6b9596
yan.gorshkov <gorshkov.yan1998@gmail.com>
2020-10-03 21:28:24 +0300

    Build issue was fixed

--- src/LeetCodeSolutions/2.add-two-numbers.cs
+++ src/LeetCodeSolutions/2.add-two-numbers.cs
@@ -44,3 +44,11 @@ public class Solution {
 }
 // @lc code=end
 
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+ }
